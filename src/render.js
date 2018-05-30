8d5293006aa4bc35e0a80f29e6e6f23e9d2f8ed2
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-05-30 18:19:39 +0900

    fix fix render function to deal with element replacement

--- src/render.js
+++ src/render.js
@@ -1,8 +1,31 @@
-function render(element, parentDom) {
-    const  { type, props }  = element;
+let rootInstance = null;
 
-    // Create DOM element
-    const isTextElement = type ===  "TEXT ELEMENT";
+
+function render(element, container) {
+    const prevInstance = rootInstance;
+    const nextInstance = reconcile(container, prevInstance, element);
+    rootInstance = nextInstance;
+}
+
+
+// Decide whether or not child replacement has occurred.
+function reconcile(parentDom, instance, element) {
+    if(instance == null) {
+        const newInstance = instantiate(element);
+        parentDom.appendChild(newInstance.dom);
+        return newInstance;
+    } else {
+        const newInstance = instantiate(element);
+        parentDom.replaceChild(newInstance.dom, instance.dom);
+        return newInstance;
+    }
+}
+
+function instantiate(element) {
+    const {type, props} = element;
+
+    // createDOM element
+    const isTextElement = type === "TEXT ELEMENT";
     const dom = isTextElement ? document.createTextNode("") : document.createElement(type);
 
     // Add event listener
@@ -15,14 +38,15 @@ function render(element, parentDom) {
     // Set properties
     const isAttribute = name => !isListener(name) && name != "children";
     Object.keys(props).filter(isAttribute).forEach(name => {
-        dom[name] =  props[name];
+        dom[name] = props[name];
     });
 
     // Render Children
     const childElements = props.children || [];
-    childElements.forEach(childElement => render(childElement, dom));
+    const childInstances = childElements.map(instantiate);
+    const childDoms = childInstances.map(childInstance => childInstance.dom);
+    childDoms.forEach(childDom => dom.appendChild(childDom));
 
-
-    // Append to parent
-    parentDom.appendChild(dom);
+    const instance = { dom, element, childInstances };
+    return instance;
 }
