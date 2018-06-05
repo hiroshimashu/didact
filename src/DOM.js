14f70adb927ae5a44a093f61a8c389571fd15345
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-06-05 15:21:53 +0900

    add add example

---
+++ src/DOM.js
@@ -0,0 +1,17 @@
+const domRoot = document.getElementById("root");
+// Create a new element given a tag name
+const domInput = document.createElement("input");
+// Set properties
+domInput["type"] = "text";
+domInput["value"] = "Hi world";
+domInput["className"] = "my-class";
+// Listen to events
+domInput.addEventListener("change", e => alert(e.target.value));
+// Create a text node
+const domText = document.createTextNode("");
+// Set text node content
+domText["nodeValue"] = "Foo";
+// Append an element
+domRoot.appendChild(domInput);
+// Append a text node (same as previous)
+domRoot.appendChild(domText);
